15216beb331d1477be26024a4bedc83f44efcc2c
MayconVM <mayconvm@gmail.com>
2018-02-15 23:31:34 -0200

    Component with slider menu.

---
+++ src/component/SliderMenu.js
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, View, ListView } from 'react-native';
+import React, { Component } from 'react';
+
+
+export default class SliderMenu extends React.Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
+
+  render() {
+    let {onPress, title} = this.props;
+
+    const styles = StyleSheet.create({
+      top: {
+        flex: 3,
+        backgroundColor: 'red',
+      },
+      menu: {
+        flex: 7,
+        backgroundColor: 'blue',
+      },
+      list: {
+        alignItems: 'flex-end'
+      }
+    });
+
+    // return (
+    //   <View>
+    //     <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+    //     <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+    //     <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+    //   </View>
+    // );
+
+    return (
+      <View style={{flex: 1}}>
+        <View style={styles.top}></View>
+
+        <View style={styles.menu}>
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(rowData) => <Text>{rowData}</Text>}
+          />
+        </View>
+
+      </View>
+    );
+  }
+};
